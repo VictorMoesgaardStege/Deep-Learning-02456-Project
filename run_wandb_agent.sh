e0175ad3dc318eacf5032ce8a97f28bb681c07fe
VictorMosgaardStege <s214995@dtu.dk>
2025-11-24 11:22:15 +0100

    Change sh to match BSUB

--- run_wandb_agent.sh
+++ run_wandb_agent.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=wandb_sweep
-#SBATCH --output=wandb_sweep-%j.out
-#SBATCH --time=24:00:00
+#BSUB -J wandb_sweep                     # Job name
+#BSUB -o wandb_sweep-%J.out              # Output log file
+#BSUB -e wandb_sweep-%J.err              # Error log file
+#BSUB -W 24:00                           # Wall time (24 hours)
+#BSUB -R "rusage[mem=32000]"             # 32 GB RAM
+#BSUB -n 4                               # 4 CPU cores
 
-# Request resources:
-#SBATCH --gres=gpu:1            # GPU = huge speed boost
-#SBATCH --cpus-per-task=4       # 4 CPU threads for data loading / numpy ops
-#SBATCH --mem=32G               # Enough memory for large 3000-unit layers
+# Request 1 GPU and run job on a GPU queue
+#BSUB -gpu "num=1:j_exclusive=yes"
+#BSUB -q gpuv100
 
-# Load modules
 module load python/3.10
 
-# Activate your venv inside project folder
 cd ~/deep_learning_02456/Deep-Learning-02456-Project
 source .venv/bin/activate
 
-# Run the wandb agent
-wandb agent s214995-danmarks-tekniske-universitet-dtu/Deep-Learning-02456-Project/4dfjifzs
+#wandb agent s214995-danmarks-tekniske-universitet-dtu/Deep-Learning-02456-Project/4dfjifzs
+wandb agent s214995-danmarks-tekniske-universitet-dtu/Deep-Learning-02456-Project/i7hp58sr
