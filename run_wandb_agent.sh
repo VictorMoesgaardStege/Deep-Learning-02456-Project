3eaee792e391dc8da13ceee49f17363e7587c4b4
VictorMosgaardStege <s214995@dtu.dk>
2025-11-24 12:20:01 +0100

    Update LSF job script to use multiple GPU queues

--- run_wandb_agent.sh
+++ run_wandb_agent.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-#BSUB -J wandb_sweep                     # Job name
-#BSUB -o wandb_sweep-%J.out              # Output log file
-#BSUB -e wandb_sweep-%J.err              # Error log file
-#BSUB -W 24:00                           # Wall time (24 hours)
-#BSUB -R "rusage[mem=32000]"             # 32 GB RAM
-#BSUB -n 4                               # 4 CPU cores
+#BSUB -J wandb_sweep
+#BSUB -o wandb_sweep-%J.out
+#BSUB -e wandb_sweep-%J.err
+#BSUB -W 24:00
+#BSUB -R "rusage[mem=32000]"
+#BSUB -n 4
 
-# Request 1 GPU and run job on a GPU queue
+# Request ANY GPU by allowing several GPU queues
 #BSUB -gpu "num=1:j_exclusive=yes"
-#BSUB -q gpuv100
+#BSUB -q "gpua10 gpua40 gpua100 gpuv100"
 
 module load python/3.10
 
 cd ~/deep_learning_02456/Deep-Learning-02456-Project
 source .venv/bin/activate
 
-#wandb agent s214995-danmarks-tekniske-universitet-dtu/Deep-Learning-02456-Project/4dfjifzs
 wandb agent s214995-danmarks-tekniske-universitet-dtu/Deep-Learning-02456-Project/i7hp58sr
